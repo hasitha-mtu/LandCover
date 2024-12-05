c19b5c107e917743ef63f9ae1ece6f19247c56c3
Hasitha Adikari <adikari.adikari@mycit.ie>
2024-12-05 12:38:56 +0000

    update data preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -11,7 +11,9 @@ import ntpath
 from utils import view_tiff
 
 
-def get_input_files(dir_path, resolution, selected_bands, selected_features):
+def get_input_files(dir_path, resolution = 10,
+                    selected_bands = ['B02', 'B03', 'B04', 'B08', 'B11', 'B12'],
+                    selected_features = ['NDVI', 'NDWI', 'NDBI', 'NDUI', 'NDDI']):
     band_files = get_band_files(dir_path, resolution, selected_bands)
     print(f"get_input_files|band_files:{band_files}")
     feature_files = get_feature_files(dir_path, selected_features)
@@ -50,10 +52,8 @@ def read_shape_files(file_path):
     gdf = gpd.read_file(file_path)
     print(f"read_shape_files|columns: {gdf.columns.values}")
     print(f"read_shape_files|gdf head: {gdf.head()}")
-    with shapefile.Reader(file_path) as shp:
-        print(shp)
-        print(shp.fields)
-        print(shp.records())
+    labels = gdf['Code_18'].values
+    print(f"read_shape_files|labels:{labels}")
 
 def resample_and_align_images(dir_path, resolution, selected_bands, selected_features, ground_truth_file):
     input_files = get_input_files(dir_path, resolution, selected_bands, selected_features)
@@ -63,6 +63,16 @@ def resample_and_align_images(dir_path, resolution, selected_bands, selected_fea
         _, input_file_name = ntpath.split(input_file)
         output_image = f"{output_dir}/{input_file_name}"
         resample_and_align(input_file, output_image, ground_truth_file)
+
+def view_shape_of_all_files(dir_path, resolution, selected_bands, selected_features, ground_truth_file):
+    input_files = get_input_files(dir_path, resolution, selected_bands, selected_features) + [ground_truth_file]
+    for input_file in input_files:
+        with rasterio.open(input_file) as src:
+            image_data = src.read()
+            image_shape = image_data.shape
+            print(f"get_input_files|input_file:{input_file}")
+            print(f"get_input_files|image_shape:{image_shape}")
+            # view_tiff(input_file, title=input_file)
 
 def resample_and_align(input_image, output_image, ground_truth):
     print(f"resample_and_align|input_image:{input_image}")
@@ -107,6 +117,33 @@ def resample_and_align(input_image, output_image, ground_truth):
     with rasterio.open(output_image, 'w', **src.meta) as dst:
         dst.write(dst_data)
 
+def get_normalized_stack(file_paths):
+    bands_and_features = []
+    for file_path in file_paths:
+        with rasterio.open(file_path) as src:
+            bands_and_features.append(src.read(1))
+    return np.stack(bands_and_features)
+
+def get_data_stack(file_paths):
+    bands_and_features = []
+    for file_path in file_paths:
+        with rasterio.open(file_path) as src:
+            bands_and_features.append(src.read(1))
+    return np.stack(bands_and_features, axis=-1)
+
+def crop_shape_file(shape_file, geojson_file):
+    shapefile = gpd.read_file(shape_file)
+    geojson = gpd.read_file(geojson_file)
+    cropped_shapefile = gpd.overlay(shapefile, geojson, how='intersection')
+    cropped_shapefile.to_file('cropped_shapefile.shp')
+
+# if __name__ == "__main__":
+#     input_shape_file = "data/land_cover/U2018_CLC2018_V2020_20u1.shp/U2018_CLC2018_V2020_20u1.shp"
+#     input_geojson_file = "config/cork2.geojson"
+#     # crop_shape_file(input_shape_file, input_geojson_file)
+#     crop_shape_file = "data/land_cover/cork2/shape_file/cropped_shapefile.shp"
+#     read_shape_files(crop_shape_file)
+
 if __name__ == "__main__":
     collection_name = "SENTINEL-2"
     resolution = 10  # Define the target resolution (e.g., 10 meters)
@@ -114,14 +151,24 @@ if __name__ == "__main__":
     download_dir = f"data/{collection_name}/{today_string}"
     bands = ['B02', 'B03', 'B04', 'B08', 'B11', 'B12']
     features = ['NDVI', 'NDWI', 'NDBI', 'NDUI', 'NDDI']
-    # get_input_files(download_dir, resolution, bands, features)
     ground_truth_file = "data/land_cover/cork2/resampled_cropped_raster.tif"
-    resample_and_align_images(download_dir, resolution, bands, features, ground_truth_file)
+    view_shape_of_all_files(download_dir, resolution, bands, features, ground_truth_file)
 
-    input_files = glob.glob(f"{download_dir}/aligned/*.tiff")
-    for input_file in input_files:
-        with rasterio.open(input_file) as src:
-            image_data = src.read()
-            image_shape = image_data.shape
-            print(f"get_input_files|input_file:{input_file}")
-            print(f"get_input_files|image_shape:{image_shape}")
+# if __name__ == "__main__":
+#     collection_name = "SENTINEL-2"
+#     resolution = 10  # Define the target resolution (e.g., 10 meters)
+#     today_string = date.today().strftime("%Y-%m-%d")
+#     download_dir = f"data/{collection_name}/{today_string}"
+#     bands = ['B02', 'B03', 'B04', 'B08', 'B11', 'B12']
+#     features = ['NDVI', 'NDWI', 'NDBI', 'NDUI', 'NDDI']
+#     # get_input_files(download_dir, resolution, bands, features)
+#     ground_truth_file = "data/land_cover/cork2/resampled_cropped_raster.tif"
+#     resample_and_align_images(download_dir, resolution, bands, features, ground_truth_file)
+#
+#     input_files = glob.glob(f"{download_dir}/aligned/*.tiff")
+#     for input_file in input_files:
+#         with rasterio.open(input_file) as src:
+#             image_data = src.read()
+#             image_shape = image_data.shape
+#             print(f"get_input_files|input_file:{input_file}")
+#             print(f"get_input_files|image_shape:{image_shape}")
