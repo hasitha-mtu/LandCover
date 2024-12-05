f4fd53c3abcc735819157e03ebf65bd3c6fd2f5b
Hasitha Adikari <adikari.adikari@mycit.ie>
2024-12-05 12:39:25 +0000

    perform basic random forest

--- classification/random_forest.py
+++ classification/random_forest.py
@@ -1,7 +1,77 @@
-from osgeo import gdal, gdal_array
+import geopandas as gpd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from datetime import date
+import glob
+import rasterio
+import rasterio.plot
+import data_preprocessing
+import matplotlib.pyplot as plt
+from rasterio.transform import from_origin
+from utils import load_cmap
 
+
+def get_labels(ground_truth_file):
+    src = rasterio.open(ground_truth_file)
+    return src.read(1)
+
+def get_normalized_stack(dir_path):
+    input_files = glob.glob(f"{dir_path}/aligned/*.tiff")
+    print(f"get_normalized_stack|input_files: {input_files}")
+    return data_preprocessing.get_normalized_stack(input_files)
+
+def get_data_stack(dir_path):
+    input_files = glob.glob(f"{dir_path}/aligned/*.tiff")
+    print(f"get_data_stack|input_files: {input_files}")
+    return data_preprocessing.get_data_stack(input_files)
+
+def train_model(labels, normalized_stack):
+    # Example: Features = bands, Labels = land cover classes
+    X = normalized_stack.reshape(-1, len(normalized_stack))  # Flattened bands
+    y = labels.flatten()  # Corresponding labels
+    print(f"train_model|X:{X.shape}")
+    print(f"train_model|y:{y.shape}")
+    # Split data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Train model
+    clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    clf.fit(X_train, y_train)
+    data_stack = get_data_stack(download_dir)
+    classified = clf.predict(X).reshape(data_stack.shape[:2])
+
+
+    cmap, legend = load_cmap(file_path = "../config/color_map.json")
+    fig, ax = plt.subplots(figsize=(20, 20))
+    ax.legend(**legend)
+    rasterio.plot.show(classified, cmap=cmap, ax=ax, title='Land Cover Classification')
+    plt.show()
+
+    src = rasterio.open(ground_truth_file)
+    with rasterio.open(
+            'classified.tif',
+            'w',
+            driver='GTiff',
+            height=classified.shape[0],
+            width=classified.shape[1],
+            count=1,
+            dtype='uint8',
+            crs=src.crs,
+            transform=src.transform,
+    ) as dst:
+        dst.write(classified, 1)
 
 if __name__ == "__main__":
-    img_ds = gdal.Open('/content/sample_images/data/LE70220491999322EDC01_stack.gtif', gdal.GA_ReadOnly)
-    roi_ds = gdal.Open('/content/sample_images/data/training_data.gtif', gdal.GA_ReadOnly)
+    collection_name = "SENTINEL-2"
+    resolution = 10  # Define the target resolution (e.g., 10 meters)
+    today_string = date.today().strftime("%Y-%m-%d")
+    download_dir = f"../data/{collection_name}/{today_string}"
+    # crop_shape_file = "../data/land_cover/cork2/shape_file/cropped_shapefile.shp"
+    ground_truth_file = "../data/land_cover/cork2/resampled_cropped_raster.tif"
+    labels = get_labels(ground_truth_file)
+    print(f"shape of labels {labels.shape}")
+    normalized_stack = get_normalized_stack(download_dir)
+    print(f"shape of normalized_stack {normalized_stack.shape}")
+    print(f"shape of normalized_stack {normalized_stack[0].shape}")
+    train_model(labels, normalized_stack)
 
